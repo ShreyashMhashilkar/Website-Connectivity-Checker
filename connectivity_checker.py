627711321e083a53e54b190daa70a2990b25b7b0
ShreyashMhashilkar <itshreyash890@gmail.com>
2022-10-11 14:08:02 +0530

    updated code

--- connectivity_checker.py
+++ connectivity_checker.py
@@ -3,12 +3,11 @@ from tkinter import *
 import tkinter as tk
 window=Tk()
 
-# Set the size of the tkinter windowdow
 window.geometry("700x350")
 window.title("Site Connectivity Checker")
 head=Label(window, text="Website Connectivity Checker", font=('Calibri 15'))
 head.pack(pady=20)
-#fucntion to check
+
 def check():
     web= (url.get())
     status_code = urllib.request.urlopen(web).getcode()
@@ -18,8 +17,7 @@ def check():
     else:
         Label(window, text="Website is Not Available", font=('Calibri 15')).place(x=260,y=200)
 
-url=tk.StringVar()# url is of string type
-Entry(window,bg="yellow", textvariable=url).place(x=200,y=80,height=30,width=280)# enter a website url
-#create a button
+url=tk.StringVar()
+Entry(window,bg="yellow", textvariable=url).place(x=200,y=80,height=30,width=280)
 Button(window,bg='skyblue', text="Check",command=check).place(x=320,y=160)
-window.mainloop()#main command
+window.mainloop()
